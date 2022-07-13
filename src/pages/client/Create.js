9fcc42e424d7eb699f5edc7109e5f35e86683910
Tomas <tomas.ibac@gmail.com>
2022-07-13 09:30:10 -0400

    Agregue el modal

--- src/pages/client/Create.js
+++ src/pages/client/Create.js
@@ -1,3 +1,5 @@
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 import Table from 'react-bootstrap/Table';
 import { RiUserAddFill } from 'react-icons/ri';
 import { Header } from '../../components/Header';
@@ -13,7 +15,56 @@ function Create() {
         <div className='w-75 mt-5'>
           <div className='text-center d-flex  justify-content-between m-3'>
             <h5>Create Client</h5>
-            <RiUserAddFill size="30px" />
+
+            <button type="button" className="" data-bs-toggle="modal" data-bs-target="#myModal">
+              <RiUserAddFill size="30px" />
+            </button>
+
+            {/*Modal*/}
+            <div className="modal" id="myModal">
+              <div className="modal-dialog">
+                <div className="modal-content">
+
+                  <div className="modal-header">
+                    <h4 className="modal-title">Modal Heading</h4>
+                    <button type="button" className="btn-close" data-bs-dismiss="modal"></button>
+                  </div>
+
+                  <div className="modal-body">
+                    <Form>
+                      <Form.Group className="mb-3 text-left" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                      </Form.Group>
+                      <Form.Group className="mb-3 text-left" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                      </Form.Group>
+                      <Form.Group className="mb-3 text-left" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                      </Form.Group>
+                      <Form.Group className="mb-3 text-left" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                      </Form.Group>
+
+                      <Form.Group className="mb-3 text-left" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                      </Form.Group>
+                      <div className="modal-footer">
+                        <button type="button" className="btn btn-danger" data-bs-dismiss="modal">Close</button>
+                        <Button variant="primary" type="submit">
+                          Submit
+                        </Button>
+                      </div>
+                    </Form>
+                  </div>
+                </div>
+              </div>
+            </div>
+
           </div>
           <Table striped bordered hover>
             <thead>
